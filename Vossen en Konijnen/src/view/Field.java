11768bff95e27ce95a79af8ad5a2459cdf5e75dd
c.g.hilbrands@gmail.com <c.g.hilbrands@gmail.com@f258b209-6029-34e0-8153-21c9e0686317>
2012-11-13 19:36:41 +0000

    translation

--- Vossen en Konijnen/src/view/Field.java
+++ Vossen en Konijnen/src/view/Field.java
@@ -10,8 +10,8 @@ import model.Randomizer;
 
 
 /**
- * Represent a rectangular grid of field positions.
- * Each position is able to store a single animal.
+ * representeerd een rechthoekig raster voor Field posities.
+ * elke positie heeft de mogelijkheid om een dier neer te zetten.
  * 
  * @author Bastiaan Vreijsen, Christian Hilbrands, Georg Duees
  * @version 2012.11.13
@@ -28,9 +28,9 @@ public class Field
     private Object[][] field;
 
     /**
-     * Represent a field of the given dimensions.
-     * @param depth The depth of the field.
-     * @param width The width of the field.
+     * representeerd een veld met de gegeven dementies.
+     * @param depth de diepte van het veld.
+     * @param width de breedte van het veld.
      */
     public Field(int depth, int width)
     {
@@ -41,7 +41,7 @@ public class Field
     }
     
     /**
-     * Empty the field.
+     * leegt het veld van dieren.
      */
     public void clear()
     {
@@ -53,8 +53,8 @@ public class Field
     }
     
     /**
-     * Clear the given location.
-     * @param location The location to clear.
+     * maakt de gegeven locatie schoon.
+     * @param location de locatie om schoon te maken.
      */
     public void clear(Location location)
     {
@@ -62,12 +62,11 @@ public class Field
     }
     
     /**
-     * Place an animal at the given location.
-     * If there is already an animal at the location it will
-     * be lost.
-     * @param animal The animal to be placed.
-     * @param row Row coordinate of the location.
-     * @param col Column coordinate of the location.
+     * plaatst een dier op een gegeven locatie.
+     * als er al een dier op de gegeven locatie staat, gaat dat dier verloren.
+     * @param animal dier wat wordt geplaatst.
+     * @param row rij coördinaten van de locatie.
+     * @param col Colum coördinaten van de locatie.
      */
     public void place(Object animal, int row, int col)
     {
@@ -75,11 +74,10 @@ public class Field
     }
     
     /**
-     * Place an animal at the given location.
-     * If there is already an animal at the location it will
-     * be lost.
-     * @param animal The animal to be placed.
-     * @param location Where to place the animal.
+     * plaats een dier op de gegeven locatie.
+     * als er al een dier op de gegeven locatie staat, gaat dat dier verloren.
+     * @param animal het te plaatsen dier.
+     * @param location de locatie om het dier te plaatsen.
      */
     public void place(Object animal, Location location)
     {
@@ -87,9 +85,9 @@ public class Field
     }
     
     /**
-     * Return the animal at the given location, if any.
-     * @param location Where in the field.
-     * @return The animal at the given location, or null if there is none.
+     * Returnt het dier op de gegeven locatie als dat er is.
+     * @param location waar in het veld.
+     * @return het dier op de gegeven locatie of null als er geen dier is.
      */
     public Object getObjectAt(Location location)
     {
@@ -97,10 +95,10 @@ public class Field
     }
     
     /**
-     * Return the animal at the given location, if any.
-     * @param row The desired row.
-     * @param col The desired column.
-     * @return The animal at the given location, or null if there is none.
+     * Returnt het dier op de gegeven locatie als dat er is.
+     * @param row de gewenste rij in het veld.
+     * @param col de gewenste colum in het veldThe desired column.
+     * @return het dier op de gegeven locatie of null als er geen dier is.
      */
     public Object getObjectAt(int row, int col)
     {
@@ -108,12 +106,12 @@ public class Field
     }
     
     /**
-     * Generate a random location that is adjacent to the
-     * given location, or is the same location.
-     * The returned location will be within the valid bounds
-     * of the field.
-     * @param location The location from which to generate an adjacency.
-     * @return A valid location within the grid area.
+     * genereer een willekeurige locatie die grenst aan de
+     * gegeven locatie of dezelfde locatie
+     * de returnt locatie zal binnen de geldende grenzen liggen
+     * van het veld.
+     * @param location de locatie.
+     * @return een geldige locatie binnen het rastergebied.
      */
     public Location randomAdjacentLocation(Location location)
     {
@@ -122,9 +120,9 @@ public class Field
     }
     
     /**
-     * Get a shuffled list of the free adjacent locations weather there is grass or not doesnt matter.
-     * @param location Get locations adjacent to this.
-     * @return A list of free adjacent locations.
+     * hier krijgt een geschude lijst van de vrije aangrenzende locaties waar gras is of het er niet toe doet.
+     * @param location krijgt locaties naast dit.
+     * @return een lijst van bruikbare aangrenzende locaties.
      */
     public List<Location> getFreeAdjacentLocations(Location location)
     {
@@ -138,9 +136,9 @@ public class Field
     }
     
     /**
-     * Get a shuffled list of the free adjacent locations.
-     * @param location Get locations adjacent to this.
-     * @return A list of free adjacent locations.
+     * krijg een geschudde lijst van vrije aangrenzende locaties.
+     * @param location krijg locaties aangrenzend aan de locatie.
+     * @return een lijst van bruikbare aangrenzende locaties.
      */
     public List<Location> getFreeGrassLocations(Location location)
     {
@@ -155,12 +153,11 @@ public class Field
     }
     
     /**
-     * Try to find a free location that is adjacent to the
-     * given location. If there is none, return null.
-     * The returned location will be within the valid bounds
-     * of the field.
-     * @param location The location from which to generate an adjacency.
-     * @return A valid location within the grid area.
+     * probeer een vrije locatie te vinden aangrenzend aan de
+     * gegeven locatie. als alle plaatsen bezet zijn returnt de methode null.
+     * de terug gevende locatie is binnen het rastergebied.
+     * @param location de locatie waarnaast wordt gegenereerd.
+     * @return een geldige locatie binnen het rastergebied.
      */
     public Location freeAdjacentLocation(Location location)
     {
@@ -175,11 +172,11 @@ public class Field
     }
 
     /**
-     * Return a shuffled list of locations adjacent to the given one.
-     * The list will not include the location itself.
-     * All locations will lie within the grid.
-     * @param location The location from which to generate adjacencies.
-     * @return A list of locations adjacent to that given.
+     * Returnt een geschudde lijst van locaties aangenzend aan de gegeven locatie.
+     * de lijst bestaat alleen uit aangrenzende locaties.
+     * alle aangrenzende locaties liggen binnen het raster.
+     * @param location de locatie waarnaast wordt gegenereerd.
+     * @return een lijst van aangrenzende locaties.
      */
     public List<Location> adjacentLocations(Location location)
     {
@@ -210,11 +207,10 @@ public class Field
     }
 
     /**
-     * Return a shuffled list of locations adjacent to the given one in a radious of 2.
-     * The list will not include the location itself.
-     * All locations will lie within the grid.
-     * @param location The location from which to generate adjacencies.
-     * @return A list of locations adjacent to that given.
+     * Returnt een geschudde lijst van aangrenzende locaties met een radius van 2.
+     * alle locaties zijn binnen het raster.
+     * @param location de locatie waarnaast wordt gegenereerd.
+     * @return een lijst met aangrenzende locaties.
      */
     public List<Location> adjacentTwoLocations(Location location)
     {
@@ -244,8 +240,8 @@ public class Field
         return locations;
     }
     /**
-     * Return the depth of the field.
-     * @return The depth of the field.
+     * Returnt de diepte van het veld(raster).
+     * @return de diepte van het veld(raster).
      */
     public int getDepth()
     {
@@ -253,15 +249,15 @@ public class Field
     }
     
     /**
-     * Return the width of the field.
-     * @return The width of the field.
+     *  Returnt de breedte van het veld(raster).
+     * @return de breedte van het veld(raster).
      */
     public int getWidth()
     {
         return width;
     }
     /**
-     * a method to see whether there is Grass at the location or not.
+     * een methode om te kijken of er gras op de locatie is of niet.
      */
     public boolean isGrass(Location location)
     {

e4d144855cce9d384d6aefeb9fd1a4df55c43851
bastiaan.vreijsen@gmail.com <bastiaan.vreijsen@gmail.com@f258b209-6029-34e0-8153-21c9e0686317>
2012-11-14 15:47:54 +0000

    main func

--- Vossen en Konijnen/src/StartVossenKonijnen.java
+++ Vossen en Konijnen/src/StartVossenKonijnen.java
@@ -31,19 +31,6 @@ public class StartVossenKonijnen extends KeyAdapter
 	 * 
 	 * @param args
 	 */
-	public static void main(String[] args) 
-	{
-		// Get default locale
-		Locale locale = Locale.getDefault();
-
-		// Set the default locale to pre-defined locale
-		Locale.setDefault(Locale.ENGLISH);
-
-		// Set the default locale to custom locale
-		locale = new Locale("en");
-		Locale.setDefault(locale);
-		averageButton();
-	}
 	private static void averageButton()
 	{
 		
--- Vossen en Konijnen/src/main.java
+++ Vossen en Konijnen/src/main.java
@@ -1,4 +1,16 @@
 
 public class main {
+	public static void main(String[] args) 
+	{
+		// Get default locale
+		Locale locale = Locale.getDefault();
 
+		// Set the default locale to pre-defined locale
+		Locale.setDefault(Locale.ENGLISH);
+
+		// Set the default locale to custom locale
+		locale = new Locale("en");
+		Locale.setDefault(locale);
+		averageButton();
+	}
 }
